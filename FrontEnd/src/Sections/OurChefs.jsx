4265890f0bf0c79416e671da3963036f95e64083
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-04-22 13:20:24 +0500

    update dummy data

--- FrontEnd/src/Sections/OurChefs.jsx
+++ FrontEnd/src/Sections/OurChefs.jsx
@@ -4,19 +4,23 @@ const OurChefs = () => {
   const chefs = [
     {
       name: "Chef John Doe",
-      image: "https://i.pinimg.com/736x/c3/59/94/c3599469bc5b54a2a70477761d77d42b.jpg",
+      image:
+        "https://i.pinimg.com/736x/c3/59/94/c3599469bc5b54a2a70477761d77d42b.jpg",
     },
     {
       name: "Chef Jane Smith",
-      image: "https://i.pinimg.com/originals/43/db/67/43db6754aeaf9819d76d8d8b9b3f1aed.jpg",
+      image:
+        "https://i.pinimg.com/originals/43/db/67/43db6754aeaf9819d76d8d8b9b3f1aed.jpg",
     },
     {
       name: "Chef Mike Brown",
-      image: "https://i.pinimg.com/originals/d7/d0/34/d7d03488b0377b5dedb81cfec61a405c.jpg",
+      image:
+        "https://i.pinimg.com/originals/d7/d0/34/d7d03488b0377b5dedb81cfec61a405c.jpg",
     },
     {
       name: "Chef Emily Davis",
-      image: "https://i.pinimg.com/videos/thumbnails/originals/d6/f9/c4/d6f9c4f7f46f1e33e073770bf60c1a43.0000000.jpg",
+      image:
+        "https://i.pinimg.com/videos/thumbnails/originals/d6/f9/c4/d6f9c4f7f46f1e33e073770bf60c1a43.0000000.jpg",
     },
   ];
   return (
