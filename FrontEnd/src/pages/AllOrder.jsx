84f386662d850d16531881eab5fa6f1a22e3c045
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-05-02 07:45:40 +0500

    orders filter done

--- FrontEnd/src/pages/AllOrder.jsx
+++ FrontEnd/src/pages/AllOrder.jsx
@@ -55,8 +55,22 @@ const AllOrder = () => {
     "delivered",
     "cancelled",
   ];
+  const orderFilters = [
+    "placed",
+    "confirmed",
+    "preparing",
+    "OnTheWay",
+    "delivered",
+    "cancelled",
+    "All",
+  ];
 
   const paymentStatusOptions = ["paid", "unpaid"];
+  const paymentFilters = ["paid", "unpaid", "All"];
+
+  const [orderStatusFilter, setOrderStatusFilter] = useState("All")
+  const [paymentStatusFilter, setPaymentStatusFilter] = useState("All")
+
 
 
   const handleOrderStatus = async (id, value) => {
@@ -137,9 +151,42 @@ const AllOrder = () => {
 
   return (
     <div className="p-6 text-white">
-      <h1 className="text-2xl font-bold mb-6">Order Management</h1>
-      {showRiders ? "" : <div className="overflow-x-auto flexCenter">
-        <table className="w-300 border border-white/10 rounded-lg">
+      <div className="flex gap-5 w-full mb-5 mt-2">
+        <div className="ProductFIlter flex gap-2">
+          <p>Order Filters By</p>
+          <select
+            value={orderStatusFilter}
+            onChange={(e) =>
+              setOrderStatusFilter(e.target.value)
+            }
+            className="bg-black border border-white/20 text-white text-sm p-1 rounded focus:outline-none"
+          >
+            {orderFilters.map((status) => (
+              <option key={status} value={status}>
+                {status}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="Deals flex gap-2">
+          <p>Payment Filters By</p>
+          <select
+            value={paymentStatusFilter}
+            onChange={(e) =>
+              setPaymentStatusFilter(e.target.value)
+            }
+            className="bg-black border border-white/20 text-white text-sm p-1 rounded focus:outline-none"
+          >
+            {paymentFilters.map((status) => (
+              <option key={status} value={status}>
+                {status}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      {showRiders ? "" : <div className="overflow-x-auto w-full flexCenter">
+        <table className="min-w-300 border border-white/10 rounded-lg">
           <thead className="bg-white/5">
             <tr>
               <th className="p-3">Order By</th>
@@ -153,97 +200,95 @@ const AllOrder = () => {
 
           <tbody>
             {orders.map((order) => (
-              <tr key={order._id} className="border-t border-white/10  align-top">
+              order.orderStatus === orderStatusFilter || order.paymentStatus === paymentStatusFilter || orderStatusFilter === "All" && paymentStatusFilter === "All" ?
+                <tr key={order._id} className="border-t border-white/10  align-top">
+                  <td className="p-3 text-xs w-40 ">
+                    <p className="text-gray-400">{order.orderBy?.name}</p>
+                    <p>{order.orderBy?.email || order.orderBy}</p>
+                    <p className="text-gray-400">{order.contactNumber}</p>
+                    <p className="text-gray-400">{order.deliveryAddress}</p>
+                    <p className="text-gray-400">City : <span className="text-md font-semibold text-green-400">{order.city}</span></p>
+                    <p className="text-gray-400">{`${order.createdAt.split("T")[0]} at ${order.createdAt.split("T")[1].slice(0, 5)}`}</p>
+                  </td>
 
 
-                <td className="p-3 text-xs w-40 ">
-
-                  <p className="text-gray-400">{order.orderBy?.name}</p>
-                  <p>{order.orderBy?.email || order.orderBy}</p>
-                  <p className="text-gray-400">{order.contactNumber}</p>
-                  <p className="text-gray-400">{order.deliveryAddress}</p>
-                  <p className="text-gray-400">City : <span className="text-md font-semibold text-green-400">{order.city}</span></p>
-                  <p className="text-gray-400">{`${order.createdAt.split("T")[0]} at ${order.createdAt.split("T")[1].slice(0, 5)}`}</p>
-                </td>
-
-
-                <td className="p-3  w-100">
-                  <div className=" gap-2 grid-cols-1 md:grid-cols-2 w-full grid">
-                    {order.items.map((item) => (
-                      <div
-                        key={item._id}
-                        className="flex bg-black/5 gap-2 items-center b p-2 rounded"
-                      >
-                        <LazyLoadImage
-                          src={item.url}
-                          alt={item.name}
-                          className="w-10 h-10 rounded object-cover"
-                        />
-                        <div className="text-md">
-                          <p>{item.name}</p>
-                          <p className="text-gray-400">
-                            {item.size} | x{item.quantity}
-                          </p>
+                  <td className="p-3  w-100">
+                    <div className=" gap-2 grid-cols-1 md:grid-cols-2 w-full grid">
+                      {order.items.map((item) => (
+                        <div
+                          key={item._id}
+                          className="flex bg-black/5 gap-2 items-center b p-2 rounded"
+                        >
+                          <LazyLoadImage
+                            src={item.url}
+                            alt={item.name}
+                            className="w-10 h-10 rounded object-cover"
+                          />
+                          <div className="text-md">
+                            <p>{item.name}</p>
+                            <p className="text-gray-400">
+                              {item.size} | x{item.quantity}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    ))}
-                  </div>
-                </td>
+                      ))}
+                    </div>
+                  </td>
 
 
-                <td className="p-3 text-[#FF4757] font-bold">
-                  Rs.{order.totalPrice}
-                </td>
+                  <td className="p-3 text-[#FF4757] font-bold">
+                    Rs.{order.totalPrice}
+                  </td>
 
-                {/* Order Status */}
-                <td className="p-3">
-                  <select
-                    value={order.orderStatus}
-                    onChange={(e) =>
-                      handleOrderStatus(order._id, e.target.value)
-                    }
-                    className="bg-black border border-white/20 p-1 rounded"
-                  >
-                    {orderStatusOptions.map((s) => (
-                      <option key={s}>{s}</option>
-                    ))}
-                  </select>
-                </td>
+                  {/* Order Status */}
+                  <td className="p-3">
+                    <select
+                      value={order.orderStatus}
+                      onChange={(e) =>
+                        handleOrderStatus(order._id, e.target.value)
+                      }
+                      className="bg-black border border-white/20 p-1 rounded"
+                    >
+                      {orderStatusOptions.map((s) => (
+                        <option key={s}>{s}</option>
+                      ))}
+                    </select>
+                  </td>
 
-                {/* Payment */}
-                <td className="p-3">
-                  <select
-                    value={order.paymentStatus}
-                    onChange={(e) =>
-                      handlePaymentStatus(order._id, e.target.value)
-                    }
-                    className="bg-black border border-white/20 p-1 rounded"
-                  >
-                    {paymentStatusOptions.map((s) => (
-                      <option key={s}>{s}</option>
-                    ))}
-                  </select>
-                </td>
+                  {/* Payment */}
+                  <td className="p-3">
+                    <select
+                      value={order.paymentStatus}
+                      onChange={(e) =>
+                        handlePaymentStatus(order._id, e.target.value)
+                      }
+                      className="bg-black border border-white/20 p-1 rounded"
+                    >
+                      {paymentStatusOptions.map((s) => (
+                        <option key={s}>{s}</option>
+                      ))}
+                    </select>
+                  </td>
 
-                {/* Assign Rider */}
-                <td className="p-3">
-                  <select
-                    value={order.orderAssignTo || ""}
-                    onChange={(e) =>
-                      handleAssignRider(order._id, e.target.value)
-                    }
-                    className="bg-black border border-white/20 p-1 rounded"
-                  >
-                    <option value="">Select Rider</option>
-                    {riders.map((r) => (
-                      <option key={r._id} value={r._id}>
-                        {r.name}
-                      </option>
-                    ))}
-                  </select>
-                </td>
+                  {/* Assign Rider */}
+                  <td className="p-3">
+                    <select
+                      value={order.orderAssignTo || ""}
+                      onChange={(e) =>
+                        handleAssignRider(order._id, e.target.value)
+                      }
+                      className="bg-black border border-white/20 p-1 rounded"
+                    >
+                      <option value="">Select Rider</option>
+                      {riders.map((r) => (
+                        <option key={r._id} value={r._id}>
+                          {r.name}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
 
-              </tr>
+                </tr> : null
             ))}
           </tbody>
         </table>
