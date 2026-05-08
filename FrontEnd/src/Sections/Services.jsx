0fa82463e6e86237f53ec7f2418a6e4650a12356
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-05-08 12:28:32 +0500

    updating some images

--- FrontEnd/src/Sections/Services.jsx
+++ FrontEnd/src/Sections/Services.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import Ready from "../assets/Ready.svg";
-import Ride from "../assets/Ride.svg";
-import Serve from "../assets/Serve.svg";    
-
-
+   
 const Services = () => {
   return (
    <div className="w-full py-12 px-5 md:px-10 mt-5 flex flex-col items-center justify-center">
@@ -20,7 +16,7 @@ const Services = () => {
 
         <div className="IconsAndText w-full flex items-center justify-evenly mt-20 md:flex-row flex-col gap-10">
           <div className="Ready flex-col gap-4 flex items-center justify-center">
-            <img src={Ready} alt="Ready" width="200" height="200" />
+            <img src="https://res.cloudinary.com/dcrkdgbd9/image/upload/v1778225232/Ready_ngl2kh.svg" alt="Ready" width="200" height="200" />
             <h1 className="text-lg font-semibold">Freshly Prepared</h1>
             <p className="text-md text-center">
               Your order is made fresh, full taste <br />
@@ -29,7 +25,7 @@ const Services = () => {
           </div>
 
           <div className="Ride flex-col gap-4 flex items-center justify-center">
-            <img src={Ride} alt="Ride" width="400" height="400" />
+            <img src="https://res.cloudinary.com/dcrkdgbd9/image/upload/v1778225232/Ride_z3cbfy.svg" alt="Ride" width="400" height="400" />
             <h1 className="text-lg font-semibold">Fast Delivery</h1>
             <p className="text-md text-center">
               Rider on the way quickly, no late scene <br />
@@ -38,7 +34,7 @@ const Services = () => {
           </div>
 
           <div className="Serve flex-col gap-4 flex items-center justify-center">
-            <img src={Serve} alt="Serve" width="400" height="400" />
+            <img src="https://res.cloudinary.com/dcrkdgbd9/image/upload/v1778225232/Serve_awblgx.svg" alt="Serve" width="400" height="400" />
             <h1 className="text-lg font-semibold">Ready to Enjoy</h1>
             <p className="text-md text-center">
               Open, eat, and enjoy with family <br />
